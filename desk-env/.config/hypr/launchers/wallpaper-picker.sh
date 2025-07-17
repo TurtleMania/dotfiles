2217d48ae7ade10a6d7c8702c1ae50897c96cffd
TurtleMania <turtlebananaman@gmail.com>
2025-07-16 20:34:46 -0400

    Add image preview to wallpaper-picker

--- desk-env/.config/hypr/launchers/wallpaper-picker.sh
+++ desk-env/.config/hypr/launchers/wallpaper-picker.sh
@@ -1,4 +1,4 @@
 cd ~/wallpapers
-selected=$(fzf)
+selected=$(fzf --preview='kitty icat --clear --transfer-mode=memory --stdin=no --place=${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}@0x0 {}')
 
 nohup ~/.config/hypr/scripts/change-wallpaper.sh $selected &> /dev/null
