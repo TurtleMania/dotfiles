71bfb1236bbc5aedfddf3adf568913b60f223b71
TurtleMania <turtlebananaman@gmail.com>
2026-03-01 13:43:51 -0500

    Turn on markdown rendering in all modes.

--- config/.config/nvim/lua/plugins/render-markdown.lua
+++ config/.config/nvim/lua/plugins/render-markdown.lua
@@ -8,6 +8,7 @@ return {
     ft = { 'markdown' },
     opts = {
         completions =  { lsp = { enabled = true } },
+        render_modes = true,
         heading = {
             width = 'block',
             left_pad = 2,
@@ -33,80 +34,3 @@ return {
         },
     }
 }
---[[
-opts = {
-    link = {
-        enabled = true,
-        render_modes = false,
-        footnote = {
-            enabled = true,
-            superscript = true,
-            prefix = '',
-            suffix = '',
-        },
-        image = '󰥶 ',
-        email = '󰀓 ',
-        hyperlink = '󰌹 ',
-        highlight = 'RenderMarkdownLink',
-        wiki = {
-            icon = '󱗖 ',
-            body = function()
-                return nil
-            end,
-            highlight = 'RenderMarkdownWikiLink',
-        },
-        custom = {
-            web = { pattern = '^http', icon = '󰖟 ' },
-            github = { pattern = 'github%.com', icon = '󰊤 ' },
-            gitlab = { pattern = 'gitlab%.com', icon = '󰮠 ' },
-            stackoverflow = { pattern = 'stackoverflow%.com', icon = '󰓌 ' },
-            wikipedia = { pattern = 'wikipedia%.org', icon = '󰖬 ' },
-            youtube = { pattern = 'youtube%.com', icon = '󰗃 ' },
-        },
-    },
-    checkbox = {
-        enabled = true,
-        render_modes = false,
-        bullet = false,
-        right_pad = 1,
-        unchecked = {
-            icon = '󰄱 ',
-            highlight = 'RenderMarkdownUnchecked',
-            scope_highlight = nil,
-        },
-        checked = {
-            icon = '󰱒 ',
-            highlight = 'RenderMarkdownChecked',
-            scope_highlight = nil,
-        },
-        custom = {
-            todo = { raw = '[-]', rendered = '󰥔 ', highlight = 'RenderMarkdownTodo', scope_highlight = nil },
-        },
-    },
-    bullet = {
-        enabled = true,
-        render_modes = false,
-        icons = { '●', '○', '◆', '◇' },
-        ordered_icons = function(ctx)
-            local value = vim.trim(ctx.value)
-            local index = tonumber(value:sub(1, #value - 1))
-            return ('%d.'):format(index > 1 and index or ctx.index)
-        end,
-        left_pad = 0,
-        right_pad = 0,
-        highlight = 'RenderMarkdownBullet',
-        scope_highlight = {},
-    },
-    quote = { icon = '▋' },
-    anti_conceal = {
-        enabled = true,
-        ignore = {
-            code_background = true,
-            sign = true,
-        },
-        above = 0,
-        below = 0,
-    },
-},
-}
---]]
